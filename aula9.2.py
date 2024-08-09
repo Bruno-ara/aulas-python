c9d147868e26049237f6286d35ab99f406430cbb
Bruno-ara <eusouobrunol0l@gmail.com>
2024-08-09 14:34:13 -0300

    Update aula9.2.py
    formula do ultimo termo.

--- aula9.2.py
+++ aula9.2.py
@@ -2,6 +2,7 @@ pt = int(input("Primeiro termo: "))
 qt = int(input("Quantidade de termos: "))
 r = int(input("Razão: "))
 s = 0 
+ut = pt + (qt - 1)*r
 for x in range(pt, qt + 1, r):
     s += x
 print(s)
