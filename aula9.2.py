064f36b3a928ea1033175785882afc3a3ba33041
Bruno-ara <eusouobrunol0l@gmail.com>
2024-08-09 14:35:57 -0300

    Update aula9.2.py

--- aula9.2.py
+++ aula9.2.py
@@ -3,6 +3,6 @@ qt = int(input("Quantidade de termos: "))
 r = int(input("Razão: "))
 s = 0 
 ut = pt + (qt - 1)*r
-for x in range(pt, qt + 1, r):
+for x in range(pt, ut + 1, r):
     s += x
 print(s)
